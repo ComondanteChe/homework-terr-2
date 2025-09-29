0e84ed82a50512782340faf40a38e591b95d85ad
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-09-29 23:02:30 +0700

    Update vms_platform.tf

--- vms_platform.tf
+++ vms_platform.tf
@@ -13,11 +13,11 @@ variable "vm_db_default_zone" {
   default     = "ru-central1-b"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
-# variable "vm_db_default_cidr" {
-#   type        = list(string)
-#   default     = ["10.0.1.0/24"]
-#   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
-# }
+variable "vm_db_default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
 
 variable "vm_db_vpc_name" {
   type        = string
