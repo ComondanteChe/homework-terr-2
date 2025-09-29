8662f4cdd17ca4eb3b412c8fcf4b7db47b181617
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-09-29 22:35:30 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,3 +39,48 @@ resource "yandex_compute_instance" "platform" {
   }
 
 }
+
+
+
+resource "yandex_vpc_network" "db" {
+  name = var.vm_db_vpc_name
+}
+
+resource "yandex_vpc_network" "db" {
+  name = var.vm_db_vpc_name
+  zone = var.vm_db_default_zone
+  network_id     = yandex_vpc_network.db.id
+  v4_cidr_blocks = var.vm_db_default_cidr
+}
+
+data "yandex_compute_image" "ubuntu_db" {
+  family = var.vm_db_image
+}
+
+resource "yandex_compute_instance" "platform_db" {
+  name        = var.vm_db_name
+  platform_id = var.vm_db_platform
+  resources {
+    cores         = var.vm_db_cores
+    memory        = var.vm_db_memory
+    core_fraction = var.vm_db_core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_db.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.db.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+
+}
