912abc82778fed4787ceab27d298dd6ccb66ebe6
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-09-30 14:40:15 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ data "yandex_compute_image" "ubuntu" {
   family = var.vm_web_image
 }
 resource "yandex_compute_instance" "platform" {
-  name        = var.vm_web_name
+  name        = local.vm_db_name
   platform_id = var.vm_web_platform
   resources {
     cores         = var.vm_web_cores
@@ -58,7 +58,7 @@ data "yandex_compute_image" "ubuntu_db" {
 }
 
 resource "yandex_compute_instance" "platform_db" {
-  name        = var.vm_db_name
+  name        = local.var.vm_db_name
   platform_id = var.vm_db_platform
   zone        = var.vm_db_default_zone
   resources {
