73ac36a4aa021ab07d9f9e2f808b72ceca47f913
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-09-30 22:26:25 +0700

    Update nat.tf

--- nat.tf
+++ nat.tf
@@ -6,7 +6,7 @@ data "yandex_vpc_network" "net" {
 resource "yandex_vpc_subnet" "subnet" {
   folder_id      = var.folder_id
   name           = "nat-network"
-  v4_cidr_blocks = ["10.20.30.0/24"]
+  v4_cidr_blocks = ["10.0.1.0/24"]
   zone           = "ru-central1-a"
   network_id     = data.yandex_vpc_network.net.id
   route_table_id = yandex_vpc_route_table.rt.id
@@ -21,7 +21,7 @@ resource "yandex_vpc_gateway" "nat_gateway" {
 resource "yandex_vpc_route_table" "rt" {
   folder_id      = var.folder_id
   name       = "test-route-table"
-  network_id = "e9b6ctfsvhq3e8qanatt"
+  network_id = "data.yandex_vpc_network.net.id"
 
   static_route {
     destination_prefix = "0.0.0.0/0"
