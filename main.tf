ed904a78bf349640e36e32ce836caeb7d4e56dc5
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-09-30 16:16:18 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ resource "yandex_compute_instance" "platform" {
   name        = local.vm_web_name
   platform_id = var.vm_web_platform
   resources {
-    cores         = var.vm_web_cores
-    memory        = var.vm_web_memory
-    core_fraction = var.vm_web_core_fraction
+    cores         = var.vms_resources["web"].cores
+    memory        = var.vms_resources["web"].memory
+    core_fraction = var.vms_resources["web"].core_fraction
   }
   boot_disk {
     initialize_params {
@@ -35,7 +35,7 @@ resource "yandex_compute_instance" "platform" {
 
   metadata = {
     serial-port-enable = 1
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+    ssh-keys           = "${var.vms_ssh_root_key["ssh"].user}:${var.vms_ssh_root_key["ssh"].ssh-keys}"
   }
 
 }
@@ -62,9 +62,9 @@ resource "yandex_compute_instance" "platform_db" {
   platform_id = var.vm_db_platform
   zone        = var.vm_db_default_zone
   resources {
-    cores         = var.vm_db_cores
-    memory        = var.vm_db_memory
-    core_fraction = var.vm_db_core_fraction
+    cores         = var.vms_resources["db"].cores
+    memory        = var.vms_resources["db"].memory
+    core_fraction = var.vms_resources["db"].core_fraction
   }
   boot_disk {
     initialize_params {
