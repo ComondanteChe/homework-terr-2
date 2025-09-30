91f30326f859f9b3781d3d939a180c6739b99f55
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-09-30 14:45:43 +0700

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-    vm_db_name  = "${var.vm_db_vpc_name}-${var.vm_db_default_zone}"
-    vm_web_name = "${var.vpc_name}-${var.default_zone}"
+    vm_db_name  = "${var.vm_db_name}-${var.vm_db_default_zone}"
+    vm_web_name = "${var.vm_web_name}-${var.default_zone}"
 }
