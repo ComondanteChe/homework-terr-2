37bd8cdf06ef2e00b3d2ed257d75196434ca2b2c
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-10-02 16:32:56 +0700

    Update nat.tf

--- nat.tf
+++ nat.tf
@@ -1,6 +1,12 @@
-data "yandex_vpc_network" "net" {
+data "yandex_vpc_network" "net_web" {
   folder_id = var.folder_id
   name      = "dafault"
+}
+
+data "yandex_vpc_network" "net_db" {
+  folder_id = var.folder_id
+  name      = "default-db"
+  
 }
 
 resource "yandex_vpc_gateway" "nat_gateway" {
@@ -12,11 +18,11 @@ resource "yandex_vpc_gateway" "nat_gateway" {
 resource "yandex_vpc_route_table" "rt_web" {
     folder_id      = var.folder_id
     name       = "route-table_web"
-    network_id = data.yandex_vpc_network.develop.id
+    network_id = data.yandex_vpc_network.net_web.id
   
   static_route {
     destination_prefix = "0.0.0.0/0"
-    gateway_id         = yandex_vpc_gateway.nat_gateway.id
+    gateway_id         = yandex_vpc_gateway.net_db.id
   }
 }
 
